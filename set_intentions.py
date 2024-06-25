5b06713f677ddc60abe9391abc89fc9c3271ce43
Matt Smida <matt@mattmail.org>
2024-06-25 17:56:51 -0400

    Format to make linter happy

--- set_intentions.py
+++ set_intentions.py
@@ -1,7 +1,8 @@
 import subprocess
 
+
 def make_tags(line, lp_idx, rp_idx):
-    """ Given a string and indexes of its right and left parens, return the 
+    """ Given a string and indexes of its right and left parens, return the
     tags as a string. """
     tags = line[:rp_idx]
     tags = tags.split(',')
@@ -16,12 +17,10 @@ def main():
             lp_idx, rp_idx = line.find('('), line.find(')')
             if lp_idx > rp_idx or (lp_idx == -1 and rp_idx > 0):
                 tags_str = make_tags(line, lp_idx, rp_idx)
-                description = line[rp_idx+1:-1].strip()
+                description = line[rp_idx + 1:-1].strip()
                 command = f'task add "{description}" {tags_str}'
-                print(command)
-                # subprocess.run(command, shell=True)
+                subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
     main()
-
