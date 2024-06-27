1f9f8f3af1074e5010ae4b1b9a7db623fb632d0a
Matt Smida <matt@mattmail.org>
2024-06-27 17:05:57 -0400

    Formatting

--- new_intentions.py
+++ new_intentions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import re
 import subprocess
+import re
 
 
 def main():
--- set_intentions.py
+++ set_intentions.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import subprocess
 import json
 from datetime import datetime
