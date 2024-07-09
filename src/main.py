2908f7c0e43215849263885d0cd0e82f0e982ace
Matt Smida <matt@mattmail.org>
2024-07-09 13:12:15 -0400

    src dir, added orchestration script

---
+++ src/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import new_intentions
+import set_intentions
+import reflect
+import tawi_utils
+import sys
+
+try:
+    func = sys.argv[1]  # new, set, reflect
+except IndexError:
+    tawi_utils.display_help()
+    func = ''
+
+if __name__ == "__main__" and func == "new":
+    new_intentions.main()
+
+elif __name__ == "__main__" and func == "set":
+    set_intentions.main()
+
+elif __name__ == "__main__" and func == "reflect":
+    reflect.main()
