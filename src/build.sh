03133fe0be4c43de7a851447174bc5794ea96e54
matt <matt@mattmail.org>
2024-07-22 15:49:37 -0400

    Added help doc to build

--- src/build.sh
+++ src/build.sh
@@ -2,4 +2,5 @@ pyinstaller \
     --add-data ~/projects/tawi/src/devotions-template.md:. \
     --add-data ~/projects/tawi/src/intentions.txt:. \
     --add-data ~/projects/tawi/src/reflections.txt:. \
+    --add-data ~/projects/tawi/src/help.txt:. \
     tawi.py
